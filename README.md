da6da0816377747d37572b3e8770a31048dbaf8b
subhya <subodhd777@gmail.com>
2018-10-18 17:58:24 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# College-Management-System
-College Management System in php
+College Management System
+===================================================
+I made this project using php and Bootstrap framework during my college session.
+ 
+ step to run the project:-
+ Download file and move or copy into C:\xampp\htdocs\<folder name>
+ Then import the database using xampp,wampp etc..
+ open the browser n type localhost/<foldername>
+ and enjoy it..
+ Thnk you...
+ 
+`Username: admin`
+
+`password: admin`
---
+++ home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="css/home.css"/>
+<title>College management system made by Subodh Deshmukh..</title>
+</head>
+
+<body>
+	<div class="panel panel-default">
+  		<div class="panel-heading"><h1>College Management System</h1></div>
+                <div class="panel-body"><br><br>
+        <div>
+            <img src="images/rtupic.jpg" width="855" style="text-align: center;" height="308" onmousedown="return false;"  />
+        </div><br><br>
+        <div class="middle_text">
+            <p><strong>Pillai HOC college of engg and technology(PHCET) is an affiliating university located in Rasayani in the state of maharashtra, India. The university was established in 1970.</strong></p>
+        </div>
+                
+        <div class="text_para_home">
+            <p>PHCET is an affiliating university. Its affiliated colleges offer the degrees of Bachelor of Engineering(BE),Bachelor of Technology (B.Tech), Master of Technology (M.Tech), Master of Business Administration (MBA)and Master of Computer Applications (MCA).
+As of december 2018, PHECT affiliates 202 colleges. B.E. degrees are offered at nine government aided institutes and 109 are private ones. MBA degrees are offered at seven government aided institutes and 122 are private ones. MCA degrees are offered in at seven government aided institutes and 25 are private ones. M.Tech is offered in 24 colleges and BHMCT in four colleges. In addition, two colleges offer Bachelor of Architecture (B.Arch) degrees. Mumbai University Publishes Courses Results Semester wise.</p>         
+        </div>   
+                </div>
+        </div>
+    
+    
+</body>
+</html>
